eeeb8bafb786561f287928f0f0ce1bf5b25771ae
Christopher Ryan Limoa <clim0061@student.monash.edu>
2022-01-02 15:09:59 +1100

    Add project files.

---
+++ RecipeVault/RecipeVault/AppShell.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace RecipeVault
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
